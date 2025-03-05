1298813f10fdc3794629e46e2308aca97d95a604
ManoharNookala <sathrobots123@gmail.com>
2025-03-06 00:37:10 +0530

    queries modified

--- models/odp/odp_iris.sql
+++ models/odp/odp_iris.sql
@@ -1,9 +1,11 @@
 {{ config(
-    materialized='incremental',
-    incremental_strategy='merge',
-    unique_key=None
+    materialized='incremental'
 ) }}
 
+SELECT DISTINCT *
+FROM `learn-436612.staging.staging_iris`
+WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
-SELECT DISTINCT *  
-FROM `learn-436612.staging.Iris`
+{% if is_incremental() %}
+  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM {{ this }})  -- Only new data
+{% endif %}
--- models/staging/staging_iris.sql
+++ models/staging/staging_iris.sql
@@ -1,12 +1,14 @@
 {{ config(
-    materialized='incremental',
-    incremental_strategy='insert_overwrite'
+    materialized='incremental'
 ) }}
 
-
-SELECT DISTINCT * 
+SELECT DISTINCT *
 FROM `learn-436612.landing.Iris`
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+
+{% if is_incremental() %}
+  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM {{ this }})  -- Only new data
+{% endif %}
 
 -- If your model uses materialized: table, dbt drops and recreates the table every time.
 -- If your model uses materialized: view, dbt replaces the view.
