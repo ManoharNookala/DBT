f58fb22d7f970b433eec0b091b7171d47d448d71
ManoharNookala <sathrobots123@gmail.com>
2025-03-06 00:46:42 +0530

    renamed file

--- models/staging/staging_iris.sql
+++ models/staging/staging_iris.sql
@@ -13,3 +13,5 @@ WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 -- If your model uses materialized: table, dbt drops and recreates the table every time.
 -- If your model uses materialized: view, dbt replaces the view.
 -- ✅ Fix: Use materialized: incremental instead.
+
+--dbt inserts data into the table that matches the model name inside your BigQuery dataset.
