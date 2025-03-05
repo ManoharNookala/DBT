b3a370da89f29e3a5b95ee0328748cb1430c2d4e
ManoharNookala <sathrobots123@gmail.com>
2025-03-06 02:41:49 +0530

    changed table names

--- models/odp/Iris.sql
+++ models/odp/Iris.sql
@@ -4,9 +4,13 @@
 ) }}
 
 SELECT DISTINCT *
-FROM `learn-436612.staging.staging_iris`
+FROM `learn-436612.staging.Iris`
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
   AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM {{ this }})  -- Only new data
 {% endif %}
+
+-- is_incremental() → Ensures only new data is inserted in future runs:
+-- First run → Loads everything from yesterday
+-- Next runs → Loads only records newer than the latest timestamp in {{ this }} (i.e., odp.{{ model_name }})
