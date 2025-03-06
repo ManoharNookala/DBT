46cc2690cabde6bab2217bd227f6c1a8c195f58d
ManoharNookala <sathrobots123@gmail.com>
2025-03-06 12:33:57 +0530

    changed table

--- models/odp/odp_Iris.sql
+++ models/odp/odp_Iris.sql
@@ -8,7 +8,7 @@ FROM `learn-436612.staging.Iris`
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
-  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM {{ `learn-436612.staging.Iris` }})
+  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM `{{learn-436612.staging.Iris` }}`)
 {% endif %}
 
 /*
--- models/staging/staging_Iris.sql
+++ models/staging/staging_Iris.sql
@@ -8,7 +8,7 @@ FROM `learn-436612.landing.Iris`
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
-  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM {{ `learn-436612.landing.Iris` }})  -- Only new data
+  AND DATE(timestamp_created) > (SELECT MAX(DATE(timestamp_created)) FROM `{{learn-436612.landing.Iris }}` )  -- Only new data
 {% endif %}
 
 -- If your model uses materialized: table, dbt drops and recreates the table every time.
