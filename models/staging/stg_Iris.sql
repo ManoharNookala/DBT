ba2d39498b51b132567841b4ce88cd9b940c240b
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 06:34:28 +0530

    adding changes

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -11,6 +11,6 @@ WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 {% if is_incremental() %}
   AND timestamp_created > (
       SELECT COALESCE(MAX(timestamp_created), TIMESTAMP('1900-01-01')) 
-      FROM {{ this }}
+      FROM {{ database }}.staging.stg_Iris
   )
 {% endif %}
