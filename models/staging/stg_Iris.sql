0359a6e918bdb21dd932a2f1a9430acee1aa45fb
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 06:19:54 +0530

    added data

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -1,15 +1,16 @@
 {{ config(
     materialized='incremental',
     schema="staging",
-    pre_hook="DELETE FROM '{{ learn-436612.staging.stg_Iris }}' WHERE TRUE"
+    pre_hook="DELETE FROM {{ database }}.{{ schema }}.stg_Iris WHERE TRUE"
 ) }}
 
 SELECT DISTINCT *
-FROM {{ ref('Iris') }}  -- ✅ Use ref() to ensure dbt resolves dependencies correctly
+FROM {{ ref('Iris') }}  
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
   AND timestamp_created > (
-      SELECT COALESCE(MAX(timestamp_created), TIMESTAMP('1900-01-01')) FROM {{ this }}  -- ✅ Ensure correct type matching
+      SELECT COALESCE(MAX(timestamp_created), TIMESTAMP('1900-01-01')) 
+      FROM {{ this }}
   )
 {% endif %}
