17c0be48381ff3bb3f676eaea7e350af99edc873
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 11:00:55 +0530

    default_schema added

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,4 +1,8 @@
 {% macro generate_schema_name(custom_schema_name, node) %}
+    -- If custom_schema_name is None, set a default value
+    {% set custom_schema_name = custom_schema_name or 'default_schema' %}
+
+    -- Remove 'odp_' prefix if present
     {% if custom_schema_name.startswith('odp_') %}
         {% set new_schema = custom_schema_name | replace('odp_', '') %}
     {% else %}
