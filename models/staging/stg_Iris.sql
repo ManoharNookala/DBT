9ec66f444ff44076802db99d48a5242faa40b5ed
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 15:50:49 +0530

    s5

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -5,10 +5,10 @@
 
 SELECT DISTINCT *
 FROM {{source('landing', 'Iris')}}
-WHERE DATE(DATE_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
-  AND DATE_created > (
+  AND timestamp_created > (
       SELECT COALESCE(DATE(MAX(DATE_created)), DATE('1900-01-01')) 
       FROM {{source('staging', 'stg_Iris')}}
   )
