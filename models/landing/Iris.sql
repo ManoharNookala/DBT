0d0677d1701bda64556b3e6f602bf441861b8a0f
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 07:26:54 +0530

    avoid circular dependency

--- models/landing/Iris.sql
+++ models/landing/Iris.sql
@@ -1,3 +1,3 @@
-select * 
-from {{ref('stg_Iris')}}
+SELECT * 
+FROM {{ source('raw', 'Iris') }}  -- Use 'source' instead of 'ref'
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
