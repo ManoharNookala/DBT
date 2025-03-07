532bafcaf68e2a0c2c2198c34c95d29694707fc2
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 11:29:46 +0530

    sources to source

--- models/odp/odp_Iris.sql
+++ models/odp/odp_Iris.sql
@@ -1,8 +1,8 @@
 {{ config(
   materialized='incremental',
   schema="odp",
-  post_hook="DELETE FROM {{sources('staging', 'stg_Iris')}} WHERE TRUE"
+  post_hook="DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE"
 ) }}
 
 SELECT DISTINCT *
-FROM {{sources('staging', 'stg_Iris')}}
+FROM {{source('staging', 'stg_Iris')}}
--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -1,16 +1,16 @@
 {{ config(
     materialized='incremental',
     schema="staging",
-    pre_hook="DELETE FROM {{sources('staging', 'stg_Iris')}} WHERE TRUE"
+    pre_hook="DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE"
 ) }}
 
 SELECT DISTINCT *
-FROM {{sources('landing', 'Iris')}}
+FROM {{source('landing', 'Iris')}}
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
   AND timestamp_created > (
       SELECT COALESCE(MAX(timestamp_created), TIMESTAMP('1900-01-01')) 
-      FROM {{sources('staging', 'stg_Iris')}}
+      FROM {{source('staging', 'stg_Iris')}}
   )
 {% endif %}
