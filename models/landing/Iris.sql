c297b1d10e860d50f80c05c7a5e578cb4e975437
ManoharNookala <sathrobots123@gmail.com>
2025-03-07 07:28:15 +0530

    c

--- models/landing/Iris.sql
+++ models/landing/Iris.sql
@@ -1,3 +1,3 @@
 SELECT * 
-FROM {{ source('raw', 'Iris') }}  -- Use 'source' instead of 'ref'
+FROM {{ source('landing', 'Iris') }}  -- Use 'source' instead of 'ref'
 WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
