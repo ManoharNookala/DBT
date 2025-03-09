68faf7b3dd6a8c58fbfd847a3f921ca61d518fe8
ManoharNookala <sathrobots123@gmail.com>
2025-03-09 12:13:08 +0530

    workflow changed

--- scripts/run_dbt.sh
+++ scripts/run_dbt.sh
@@ -11,18 +11,19 @@ export ODP_DATASET=${ODP_DATASET}
 export GCP_CREDENTIALS=${GCP_CREDENTIALS}
 
 # Activate Python Virtual Environment (Optional)
-# source venv/bin/activate  # Uncomment if using a virtual environment
+# Uncomment if using a virtual environment
+# source venv/bin/activate  
 
 # Install dbt dependencies
 python -m pip install --upgrade pip
 pip install dbt-core==1.7.18 dbt-bigquery==1.7.9
 
 # Run dbt commands
-dbt debug --profiles-dir ./profile --target $DBT_TARGET
-dbt compile --profiles-dir ./profile --target $DBT_TARGET
-dbt run --profiles-dir ./profile --target staging    # Landing → Staging
-dbt run --profiles-dir ./profile --target odp_final  # Staging → ODP
-dbt test --profiles-dir ./profile --target $DBT_TARGET
+dbt debug --profiles-dir ./profiles --target $DBT_TARGET
+dbt compile --profiles-dir ./profiles --target $DBT_TARGET
+dbt run --profiles-dir ./profiles --target staging    # Landing → Staging
+dbt run --profiles-dir ./profiles --target odp_final  # Staging → ODP
+dbt test --profiles-dir ./profiles --target odp_final  # Testing final dataset
 
 # Cleanup credentials
 rm -f $HOME/service_account.json
