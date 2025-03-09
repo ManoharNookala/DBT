20569e0fc345968a1c1ff7dcffce2754dd23cc54
ManoharNookala <sathrobots123@gmail.com>
2025-03-09 12:51:38 +0530

    taken older

--- macros/generate_schema_name.sql
+++
@@ -1,7 +0,0 @@
-{% macro generate_schema_name(custom_schema_name, node) %}
-  {% if target.name == 'staging' %}
-    return 'staging_dataset'
-  {% elif target.name == 'production' %}
-    return 'final_dataset'
-  {% endif %}
-{% endmacro %}
