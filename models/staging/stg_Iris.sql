e37a1e05ce1434e3c76c21c06f81c5d6f3f92d71
ManoharNookala <sathrobots123@gmail.com>
2025-03-10 18:57:43 +0530

    Queries corrected

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -15,20 +15,16 @@
   
 ) }}
 
-
-
-
 -- ,
 --   post_hook="DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE"
 
 SELECT DISTINCT *
-FROM {{source('staging', 'stg_Iris')}}
---{{ ref('stg_Iris') }}
---{{source('staging', 'stg_Iris')}}
- 
+FROM {{source('landing', 'Iris')}}
+WHERE DATE(timestamp_created) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+
 {% if is_incremental() %}
-  where timestamp_created > (
+  AND timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
-      FROM {{source('odp', 'odp_Iris')}}
+      FROM {{source('staging', 'stg_Iris')}}
   )
 {% endif %}
