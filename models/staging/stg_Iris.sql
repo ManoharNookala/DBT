703c1acfac10d1f3ff27206470d86743822f8017
ManoharNookala <sathrobots123@gmail.com>
2025-03-10 08:35:43 +0530

    used ref

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -12,7 +12,8 @@ WHERE DATE(timestamp_created) = DATE_SUB('2025-03-07', INTERVAL 1 DAY)
 {% if is_incremental() %}
   AND timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
-      FROM {{this}}
+      FROM {{ ref('stg_Iris') }}
+      --{{this}}
       --{{source('staging', 'stg_Iris')}}
   )
 {% endif %}
