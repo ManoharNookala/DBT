87902db93c6d944460e626b7f531798f3f4c19ca
ManoharNookala <sathrobots123@gmail.com>
2025-03-10 17:57:07 +0530

    consent removal

--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -1,17 +1,32 @@
 {{ config(
-    materialized='incremental'
+  materialized='incremental',
+  pre_hook = "
+            DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE;
+
+            DELETE FROM {{source('landing', 'Iris')}}
+            WHERE order_id IN (
+              SELECT order_id FROM {{source('landing', 'Consent_Removal')}}
+            );
+
+            DELETE FROM {{source('odp', 'odp_Iris')}}
+            WHERE order_id IN (
+              SELECT order_id FROM {{source('landing', 'Consent_Removal')}}
+            );"
+  
 ) }}
 
+
 -- ,
---     pre_hook="DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE"
+--   post_hook="DELETE FROM {{source('staging', 'stg_Iris')}} WHERE TRUE"
 
 SELECT DISTINCT *
-FROM {{source('landing', 'Iris')}}
-WHERE DATE(timestamp_created) = DATE_SUB('2025-03-07', INTERVAL 1 DAY)
-
+FROM {{source('staging', 'stg_Iris')}}
+--{{ ref('stg_Iris') }}
+--{{source('staging', 'stg_Iris')}}
+ 
 {% if is_incremental() %}
-  AND timestamp_created > (
+  where timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
-      FROM {{source('staging', 'stg_Iris')}}
+      FROM {{source('odp', 'odp_Iris')}}
   )
 {% endif %}
