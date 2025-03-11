d73f909e476c99f956d743f03ddf6b3acf39bd04
ManoharNookala <sathrobots123@gmail.com>
2025-03-12 00:02:31 +0530

    target_profiles

--- models/odp/odp_Iris.sql
+++ models/odp/odp_Iris.sql
@@ -3,23 +3,12 @@
     enabled=(target.profile == 'DBT_odp')
 ) }}
 
-{% if target.profile == 'DBT_odp' %}
-
 SELECT DISTINCT *
 FROM {{ source('staging', 'stg_Iris') }}
- 
 
 {% if is_incremental() %}
    WHERE timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
-      FROM {{ source('odp', 'odp_Iris') }}
-      --{{ ref('odp_Iris') }}  -- ✅ Use ref() instead of source()
+      FROM {{ ref('odp_Iris') }}  -- ✅ Correct way to reference a dbt model
   )
-{% endif %}
-
-
-
-{% else %}
-    -- Skip execution if the profile is not 'DBT_staging'
-    SELECT * FROM {{ source('landing', 'Iris') }} WHERE FALSE
 {% endif %}
--- models/staging/stg_Iris.sql
+++ models/staging/stg_Iris.sql
@@ -1,23 +1,15 @@
 {{ config(
     materialized='incremental',
-    enabled=(target.profile == 'DBT_staging'),
+    enabled=(target.profile == 'DBT_staging')
 ) }}
-
-{% if target.profile == 'DBT_staging' %}
 
 SELECT DISTINCT *
 FROM {{ source('landing', 'Iris') }}
-WHERE DATE(timestamp_created) > DATE_SUB('2025-03-06', INTERVAL 1 DAY)
+WHERE DATE(timestamp_created) > DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
 
 {% if is_incremental() %}
   AND timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
-      FROM {{ this }}  -- ✅ Use ref() instead of source()
+      FROM {{ this }}
   )
-{% endif %}
-
-
-{% else %}
-    -- Skip execution if the profile is not 'DBT_staging'
-    SELECT * FROM {{ source('landing', 'Iris') }} WHERE FALSE
 {% endif %}
