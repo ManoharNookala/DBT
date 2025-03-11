8bd0403ae1ccc3abb02c027049c574418d713716
ManoharNookala <sathrobots123@gmail.com>
2025-03-11 16:43:26 +0530

    default to odp

--- models/odp/odp_Iris.sql
+++ models/odp/odp_Iris.sql
@@ -4,10 +4,10 @@
 
 SELECT DISTINCT *
 FROM {{ source('staging', 'stg_Iris') }}
-WHERE DATE(timestamp_created) >= DATE_SUB('2024-03-06', INTERVAL 1 DAY)
+ 
 
 {% if is_incremental() %}
-  AND timestamp_created > (
+   WHERE timestamp_created > (
       SELECT COALESCE(DATE(MAX(timestamp_created)), DATE('1900-01-01')) 
       FROM {{ this }}  -- ✅ Use ref() instead of source()
   )
